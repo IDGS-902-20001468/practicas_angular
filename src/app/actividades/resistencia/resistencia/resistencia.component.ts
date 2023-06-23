20eb02c42e9f43ba75ceffd386f1cbee1663ac8c
IDGS-902-20001468 <20001468@alumnos.utleon.edu.mx>
2023-06-22 23:14:09 -0600

    BGJS actividades 22 de junio

--- src/app/actividades/resistencia/resistencia/resistencia.component.ts
+++ src/app/actividades/resistencia/resistencia/resistencia.component.ts
@@ -7,10 +7,6 @@ import { Component } from '@angular/core';
 })
 export class ResistenciaComponent {
 
-  c1:string ='';
-  c2:string ='';
-  c3:string ='';
-  color:string = '';
   v: number = 0;
   v1: string = '';
   v2: string = '';
@@ -27,28 +23,25 @@ export class ResistenciaComponent {
     'oro'
   ]
   colores = [
-    'negro',
-    'cafe',
-    'rojo',
-    'naranja',
-    'amarillo',
-    'verde',
-    'azul',
-    'violeta',
-    'gris',
-    'blanco'
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white'
   ];
 
   calcularVar():void{
     this.obtenervalor(this.c1Seleccionado)
     this.v1 = String(this.v)
-    this.c1 = this.color
     this.obtenervalor(this.c2Seleccionado)
     this.v2 = String(this.v)
-    this.c2 = this.color
     this.obtenervalor(this.c3Seleccionado)
     this.valor = Number(this.v1+this.v2)*this.v3
-    this.c3 = this.color
     this.calcular()
 
   };
@@ -66,53 +59,43 @@ export class ResistenciaComponent {
 
   obtenervalor(c:string):void{
       switch(c){
-        case 'negro':
-          this.color = 'black'
+        case 'black':
           this.v3 = 1
           this.v = 0
           break;
-          case 'cafe':
-            this.color = 'brown'
+          case 'brown':
             this.v3 = 10
             this.v = 1
             break;
-          case 'rojo':
-            this.color = 'red'
+          case 'red':
             this.v3 = 100
             this.v = 2
             break;
-          case 'naranja':
-            this.color = 'orange'
+          case 'orange':
             this.v3 = 1000
             this.v = 3
             break;
-          case 'amarillo':
-            this.color = 'yellow'
+          case 'yellow':
             this.v3 = 10000
             this.v = 3
             break;
-          case 'verde':
-            this.color = 'green'
+          case 'green':
             this.v3 = 100000
             this.v = 4
             break;
-          case 'azul':
-            this.color = 'blue'
+          case 'blue':
             this.v3 = 1000000
             this.v = 5
             break;
-          case 'violeta':
-            this.color = 'violet'
+          case 'violet':
             this.v3 = 10000000
             this.v = 6
             break;
-          case 'gris':
-            this.color = 'grey'
+          case 'grey':
             this.v3 = 100000000
             this.v = 7
             break;
-          case 'blanco':
-            this.color = 'white'
+          case 'white':
             this.v3 = 1000000000
             this.v = 8
             break;
